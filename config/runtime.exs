b20beb5f8960040f1763a9db17c9dff938127618
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-01-16 13:07:40 -0500

    set up docker

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,17 +33,13 @@ if config_env() == :prod do
   # to check this value into version control, so we use an environment
   # variable instead.
   secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+    "not secret at all because I am running at home but it needs to be at least 64 bytes"
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "0.0.0.0"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :autochecker, AutocheckerWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
@@ -52,7 +48,12 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    render_errors: [
+      formats: [html: AutocheckerWeb.ErrorHTML, json: AutocheckerWeb.ErrorJSON],
+      layout: false
+    ],
+    secret_key_base: secret_key_base,
+    check_origin: false
 
   # ## SSL Support
   #
