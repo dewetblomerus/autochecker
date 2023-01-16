8c7dd2d132098657b5d4c6585009d569f3780f01
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-01-16 10:12:11 -0500

    mix phx.new autochecker --no-ecto

---
+++ lib/autochecker/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Autochecker.Mailer do
+  use Swoosh.Mailer, otp_app: :autochecker
+end
---
+++ lib/autochecker_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule AutocheckerWeb.Layouts do
+  use AutocheckerWeb, :html
+
+  embed_templates "layouts/*"
+end
